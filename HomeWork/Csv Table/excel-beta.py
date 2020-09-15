626cc58bfb76df151921d2edbfad9a1e80f4dc07
TheFox1998 <MZrG3gzjLBUA>
2020-09-15 23:20:00 +0300

    feat(all): Init

---
+++ HomeWork/Csv Table/excel-beta.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt5 import QtWidgets, uic
+from tabulate import tabulate
+import csv
+
+
+def load_csv_file_func():
+    """Upload csv file"""
+    filename = QtWidgets.QFileDialog.getOpenFileName(main_window, 'Открыть таблицу', '', "Csv Files (*.csv)")[0]
+    try:
+        with open(filename, 'r') as csv_file:
+            csv_table_func(csv_file)
+    except:
+        pass
+
+
+def csv_table_func(file_object):
+    reader = csv.reader(file_object, delimiter=',')
+
+    all_table = [line for line in reader]
+    print(all_table)
+
+    main_window.table.setHorizontalHeaderLabels(all_table[0])
+    count_collumn = len(all_table[0])
+    count_row = len(all_table) - 1
+    print(count_row)
+    main_window.table.setColumnCount(count_collumn)
+    main_window.table.setRowCount(count_row)
+    without_headers = all_table[1:]
+    print(without_headers)
+    count = 0
+    while count < count_row:
+        for column in range(0, count_collumn, 1):
+            main_window.table.setItem(count, column, QtWidgets.QTableWidgetItem(without_headers[count][column]))
+        count += 1
+
+    main_window.table.resizeColumnsToContents()
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+main_window = uic.loadUi('MainWindow.ui')
+
+main_window.table.setColumnCount(10)
+main_window.load_button.clicked.connect(load_csv_file_func)
+
+main_window.show()
+
+app.exec_()
---
+++ HomeWork/Wiki/WikiMain.py
@@ -0,0 +1,73 @@
+import sys
+import requests
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPixmap
+import wikipedia
+from PIL import Image
+lang = 'en'
+
+def search_func():
+    wikipedia.set_lang(lang)
+    try:
+        query = main_window.search_entry.text()
+        answer = wikipedia.WikipediaPage(query)
+        image = [word for word in filter(lambda x: x.endswith('.jpg', ), answer.images)]
+        response = requests.get(image[0], stream=True)
+        with open('image.png', 'wb') as image:
+            image.write(response.content)
+        Image.open('image.png').save('image.png')
+        pixmap = QPixmap('image.png')
+        main_window.heading_label.setText(answer.title)
+        main_window.text_label.setText(answer.summary)
+        main_window.image_label.setPixmap(pixmap)
+
+    except:
+        QtWidgets.QMessageBox.critical(main_window, 'Ошибка', 'Данной статьи не существует')
+
+
+def save_doc():
+    with open('Wiki article.txt', 'w', encoding='utf-8') as file:
+        file.write(main_window.heading_label.text() + '\n' + main_window.text_label.text())
+    QtWidgets.QMessageBox.information(main_window, 'Информация', 'Файл сохранён')
+
+
+def change_on_en_func():
+    global lang
+    lang = 'en'
+
+
+def change_on_de_func():
+    global lang
+    lang = 'de'
+
+
+def change_on_fr_func():
+    global lang
+    lang = 'fr'
+
+
+def change_on_pl_func():
+    global lang
+    lang = 'pl'
+
+
+def change_on_ru_func():
+    global lang
+    lang = 'ru'
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+main_window = uic.loadUi('Main_Window.ui')
+
+main_window.search_button.clicked.connect(search_func)
+main_window.save_button.clicked.connect(save_doc)
+main_window.action.triggered.connect(change_on_en_func)
+main_window.action_2.triggered.connect(change_on_de_func)
+main_window.action_3.triggered.connect(change_on_ru_func)
+main_window.action_4.triggered.connect(change_on_pl_func)
+main_window.action_5.triggered.connect(change_on_fr_func)
+
+main_window.show()
+
+app.exec_()
